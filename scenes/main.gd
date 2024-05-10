821a6f1e3247c2929c91f6517c33b3290d0d52d2
Vignesh <vignesh77024@gmail.com>
2024-05-10 17:03:30 +0530

    Striker velocity changes according to the distance from the mouse & can reset it's position with R-Click

---
+++ scenes/main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _integrate_forces(_delta):
+	##print($Striker.linear_velocity)
+	#if Input.is_action_just_pressed("start_striker"):
+		##if $Striker.linear_velocity = Vector2.ZERO:
+		#
+		##$Striker.apply_central_impulse(-$Striker.linear_velocity)
+		##$Striker.freeze = true
+		##$Striker.set_physics_process(false)
+		#$Striker.position = $StrikerStartMarker.position
+		##$Striker.set_physics_process(true)
+		##$Striker.freeze = false
--- scenes/striker.gd
+++ scenes/striker.gd
@@ -1,19 +1,18 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
+func _physics_process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		var mouse_position = get_viewport().get_mouse_position()
-		print(get_viewport().get_mouse_position())
-		#direction, magnitude
-		# direction: mouse position
-		# magnitude: constant (for now)
+		#print(get_viewport().get_mouse_position())
 		var direction = (mouse_position - position).normalized() 
-		apply_central_impulse(direction * -500)
+		var strength = mouse_position.distance_to(position)
+		apply_central_impulse(direction * -(strength * 5))
+		print(strength)
+		
+func _integrate_forces(state):
+	if Input.is_action_just_pressed("start_striker"):
+		print("Start striker")
+		#freeze = true
+		linear_velocity = Vector2.ZERO
+		position = $"../StrikerStartMarker".position
+		#freeze = false
